04f8c2955ea82fde8d1fff52fed268ea7aebd6e8
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-05-15 20:35:37 +0530

    Wed May 15 08:35:37 PM IST 2024

--- lua/shashank/remaps.lua
+++ lua/shashank/remaps.lua
@@ -19,3 +19,7 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+--resizing for splits
+vim.keymap.set("n", "<M-j>", ":resize -2<CR>")
+vim.keymap.set("n", "<M-k>", ":resize +2<CR>")
