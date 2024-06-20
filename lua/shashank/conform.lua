f199aed03bc72d564629bdbf6e5db4f2b04dd87b
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-06-20 23:54:14 +0530

    Thu Jun 20 11:54:14 PM IST 2024

--- lua/shashank/conform.lua
+++ lua/shashank/conform.lua
@@ -2,8 +2,8 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		rust = { "rustfmt" },
-		typescript = { "prettier" },
-		typescriptreact = { "prettier" },
+		typescript = { "prettierd" },
+		typescriptreact = { "prettierd" },
 	},
 	notify_on_error = false,
 	format_on_save = {
